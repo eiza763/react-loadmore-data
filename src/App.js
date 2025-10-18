cdcb9daed43efebd3e847531921ece062b5e0a6e
eiza <eizaimtiaz3@gmail.com>
2025-10-18 10:51:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { useState } from "react";
-{/*import Accordion from "./Components/Accordion.jsx";
-import Accordion from "./Components/random-color.jsx";*/}
+
 import LoadMoreData from "./index.jsx";
 
-//import reactLogo from "./assets/react.svg";
-//import viteLogo from "/vite.svg";
-//import "./App.css";
 
 function App() {
 
